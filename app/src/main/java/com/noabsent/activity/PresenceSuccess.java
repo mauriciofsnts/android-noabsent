97332d855329988e3a69b48b0da8f11319e0dd07
Mauricio Ferraz dos Santos <74746082+mauriciofsnts@users.noreply.github.com>
2021-04-27 19:44:34 -0300

    Criando tela de presença confirmada

---
+++ app/src/main/java/com/noabsent/activity/PresenceSuccess.java
@@ -0,0 +1,18 @@
+package com.noabsent.activity;
+
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.noabsent.R;
+
+public class PresenceSuccess extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_presence_success);
+    }
+
+
+}
