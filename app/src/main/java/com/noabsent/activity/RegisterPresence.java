95648460df707bc1b53dd62c6a8cacdacbcd59a3
Mauricio Ferraz dos Santos <74746082+mauriciofsnts@users.noreply.github.com>
2021-05-03 22:22:08 -0300

    Removendo bug da localização ao carregar o app

--- app/src/main/java/com/noabsent/activity/RegisterPresence.java
+++ app/src/main/java/com/noabsent/activity/RegisterPresence.java
@@ -5,6 +5,7 @@ import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.content.Context;
@@ -32,16 +33,17 @@ import com.noabsent.dao.CourseDAO;
 import java.text.ParseException;
 
 
-public class RegisterPresence extends AppCompatActivity {
+public class RegisterPresence extends AppCompatActivity implements LocationListener {
 
     AutoCompleteTextView autoCompleteTextView;
     Button buttonPresence;
+    TextView courseTextHourInfo;
     TextView txtLatitude;
     TextView txtLongitude;
-    TextView courseTextHourInfo;
-    ConstraintLayout progressContainer;
 
-    // this annotation because this core drequires android jelly bean
+    private final int GPS_REQUEST = 100;
+    private LocationManager locationManager;
+
     @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,11 +52,12 @@ public class RegisterPresence extends AppCompatActivity {
         setContentView(R.layout.activity_register_presence);
 
         CourseDAO courseDAO = new CourseDAO();
-        buttonPresence = (Button)findViewById(R.id.buttonPresence);
+        buttonPresence = (Button) findViewById(R.id.buttonPresence);
         autoCompleteTextView = findViewById(R.id.dropdownSelectClass);
-        txtLatitude = (TextView) findViewById(R.id.txtLatitude);
-        txtLongitude = (TextView) findViewById(R.id.txtLongitude);
         courseTextHourInfo = (TextView) findViewById(R.id.courseTextHourInfo);
+        txtLatitude = findViewById(R.id.txtLatitude);
+        txtLongitude = findViewById(R.id.txtLongitude);
+
 
         String[] option = {"FUNDAMENTOS DE INTELIGÊNCIA ARTIFICIAL", "TRABALHO DE GRADUAÇÃO INTERDISCIPLINAR I", "LINGUAGENS FORMAIS E AUTÔMATOS", "PROGRAMAÇÃO PARA DISPOSITIVOS MÓVEIS"};
         ArrayAdapter arrayAdapter = new ArrayAdapter(this, R.layout.option, option);
@@ -66,7 +69,7 @@ public class RegisterPresence extends AppCompatActivity {
                 Course course = courseDAO.getCourseByName(option[position]);
 
 
-                courseTextHourInfo.setText(course.getDayOfWeek()  + " , " + course.getStartTime() + " - " + course.getEndTime());
+                courseTextHourInfo.setText(course.getDayOfWeek() + " , " + course.getStartTime() + " - " + course.getEndTime());
             }
         });
 
@@ -76,24 +79,23 @@ public class RegisterPresence extends AppCompatActivity {
 
                 Editable selectedAutoComplete = autoCompleteTextView.getText();
 
-                if(selectedAutoComplete != null && selectedAutoComplete.length() > 0 ) {
+                if (selectedAutoComplete != null && selectedAutoComplete.length() > 0) {
                     Course course = courseDAO.getCourseByName(selectedAutoComplete.toString());
 
                     try {
-                        if (courseDAO.checkCourse(course)){
+                        if (courseDAO.checkCourse(course)) {
 
                             String faculLat = "-23.536";
                             String faculLong = "-46.560";
                             String userLat = (String) txtLatitude.getText();
                             String userLong = (String) txtLongitude.getText();
 
-                            if(userLat != null && userLong != null){
-                                if (userLat.substring(10, 17).equals(faculLat) && userLong.substring(11, 18).equals(faculLong)){
+                            if (userLat != null && userLong != null) {
+                                if (userLat.substring(10, 17).equals(faculLat) && userLong.substring(11, 18).equals(faculLong)) {
                                     Intent intent = new Intent(RegisterPresence.this, PresenceSuccess.class);
                                     intent.putExtra("course", course.getName());
                                     startActivity(intent);
-                                }
-                                else {
+                                } else {
                                     Context context = getApplicationContext();
                                     CharSequence text = "Você não está na faculdade";
                                     int duration = Toast.LENGTH_SHORT;
@@ -104,8 +106,7 @@ public class RegisterPresence extends AppCompatActivity {
                             }
 
 
-                        }
-                        else {
+                        } else {
                             Context context = getApplicationContext();
                             CharSequence text = "Não há aula nesse horário";
                             int duration = Toast.LENGTH_SHORT;
@@ -118,8 +119,7 @@ public class RegisterPresence extends AppCompatActivity {
                     }
 
 
-                }
-                else {
+                } else {
                     Context context = getApplicationContext();
                     CharSequence text = "Escolha uma matéria";
                     int duration = Toast.LENGTH_SHORT;
@@ -132,42 +132,49 @@ public class RegisterPresence extends AppCompatActivity {
         });
 
 
-        // localizacao TextView
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
-        }
+        if(ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED){
 
-        else {
-            try {
-                progressContainer = (ConstraintLayout) findViewById(R.id.progressContainer);
-
-
-                LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-
-                LocationListener locationListener = new LocationListener() {
-                    @Override
-                    public void onLocationChanged(@NonNull Location location) {
-                        TextView txtLatitude = findViewById(R.id.txtLatitude);
-                        TextView txtLongitude = findViewById(R.id.txtLongitude);
-
-                        txtLatitude.setText("Latitude: " + location.getLatitude());
-                        txtLongitude.setText("Longitude: " + location.getLongitude());
-                        progressContainer.setVisibility(View.INVISIBLE);
-                    }
-                };
-
-                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1, locationListener);
-
-            }
-             catch (SecurityException ex ) {
-                 Toast.makeText(this, ex.getMessage(), Toast.LENGTH_LONG).show();
-             }
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION}, GPS_REQUEST);
+        }else{
+            getLocation();
         }
 
 
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case GPS_REQUEST: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    getLocation();
+                }
+            }
+        }
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    public void getLocation() {
+        try {
+            locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                    1000, 0, this);
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        }
+    }
 
 
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        buttonPresence.setEnabled(true);
+        txtLatitude.setText("Latitude: " + location.getLatitude());
+        txtLongitude.setText("Longitude: " + location.getLongitude());
+    }
 }
